524ab51ab5f53d91f994fb8f5a7af3a02a13a188
Daniel González <daa.gonzalez@gmail.com>
2024-09-06 20:09:05 -0500

    feat: Svelte version (#1)
    * feat: scaffolding project
    
    * feat: adapted existing page into svelte
    
    * fix: using global styles and changed default title
    
    * chore: added gh-pages package

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Header from './Header.svelte';
+  import Footer from './Footer.svelte';
+</script>
+
+<main>
+  <Header name="Daniel Gonzalez" jobTitle="Software Engineer" />
+  
+  <section id="about">
+    <h2>About Me</h2>
+    <p>Hello! I'm Daniel Gonzalez, a passionate Software Engineer with a knack for developing innovative solutions to complex problems. 
+    With a strong foundation in software development and a love for learning new technologies, I strive to create impactful software 
+    that makes a difference.</p>
+  </section>
+  
+  <Footer />
+</main>
+
+<style>
+  section {
+    padding: 20px;
+  }
+</style>
