adb4cfd8ece7d1ae2f39b06ab0ee6fd282c45851
Daniel Gonzalez <daa.gonzalez@gmail.com>
2024-09-08 13:32:44 -0500

    refactor: use tailwind on main component

---
+++ src/Home.svelte
@@ -0,0 +1,12 @@
+<div>
+    <h1 class="text-3xl font-bold">About Me</h1>
+</div>
+<div>
+    <p>
+        Hello! I'm Daniel Gonzalez, a passionate Software Engineer with a knack
+        for developing innovative solutions to complex problems. With a strong
+        foundation in software development and a love for learning new
+        technologies, I strive to create impactful software that makes a
+        difference.
+    </p>
+</div>
