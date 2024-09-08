139980663cf0be004721fdf97d50d2414ffee0ed
Daniel Gonzalez <daa.gonzalez@gmail.com>
2024-09-08 13:13:00 -0500

    refactor: rewrite navbar using tailwind

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,49 +1,27 @@
 <script>
-    import {scrollTo} from 'svelte-scrolling';
+    import { scrollTo } from "svelte-scrolling";
+
+    const navItems = [
+        { name: "Home", target: "home" },
+        { name: "Projects", target: "projects" },
+        { name: "Contact", target: "contact" },
+    ];
 </script>
+
 <nav>
-    <ul>
-        <li>
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <a use:scrollTo={'home'}> Home </a>
-        </li>
-        <li>
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <a use:scrollTo={'projects'}> Projects </a>
-        </li>
-        <li>
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <a use:scrollTo={'contact'}> Contact </a>
-        </li>
+    <ul
+        class="list-none flex justify-center fixed top-0 w-full bg-red-900/80 p-2.5 m-0 z-50"
+    >
+        {#each navItems as item}
+            <li class="mx-4 my-0">
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <a
+                    class="text-white hover:text-black font-bold cursor-pointer no-underline"
+                    use:scrollTo={item.target}
+                >
+                    {item.name}
+                </a>
+            </li>
+        {/each}
     </ul>
 </nav>
-
-<style>
-    nav ul {
-        list-style: none;
-        display: flex;
-        justify-content: center;
-        position: fixed;
-        top: 0;
-        width: 100%;
-        background-color: rgba(117, 55, 66, 0.8);
-        padding: 10px;
-        margin: 0;
-        z-index: 1000;
-    }
-
-    nav ul li {
-        margin: 0 15px;
-    }
-
-    nav ul li a {
-        text-decoration: none;
-        color: #fff;
-        font-weight: bold;
-        cursor: pointer;
-    }
-
-    nav ul li a:hover {
-        color: #dfdddd;
-    }
-</style>
