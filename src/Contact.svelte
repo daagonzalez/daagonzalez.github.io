d88dae7dc6c31c96df38c8ea2180aec2f116ab88
Daniel Gonzalez <daa.gonzalez@gmail.com>
2024-09-08 14:44:02 -0500

    feat: add background to title

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -1,6 +1,6 @@
-<div class="w-1/5">
+<div class="w-1/5 bg-white text-black p-4">
     <h1 class="text-3xl font-bold">Contact</h1>
 </div>
-<div class="w-4/5">
+<div class="w-4/5 p-4">
     Coming Soon.
 </div>
