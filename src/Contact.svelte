d6213eef729ea71637c09bd167bd71945b5fdd20
Daniel Gonzalez <daa.gonzalez@gmail.com>
2024-09-08 14:05:45 -0500

    feat: section styling

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -1,1 +1,6 @@
-Coming Soon
+<div class="w-1/5">
+    <h1 class="text-3xl font-bold">Contact</h1>
+</div>
+<div class="w-4/5">
+    Coming Soon.
+</div>
--- src/Home.svelte
+++ src/Home.svelte
@@ -1,12 +1,9 @@
-<div>
+<div class="w-1/5">
     <h1 class="text-3xl font-bold">About Me</h1>
 </div>
-<div>
-    <p>
-        Hello! I'm Daniel Gonzalez, a passionate Software Engineer with a knack
-        for developing innovative solutions to complex problems. With a strong
-        foundation in software development and a love for learning new
-        technologies, I strive to create impactful software that makes a
-        difference.
-    </p>
+<div class="w-4/5">
+    Hello! I'm Daniel Gonzalez, a passionate Software Engineer with a knack for
+    developing innovative solutions to complex problems. With a strong
+    foundation in software development and a love for learning new technologies,
+    I strive to create impactful software that makes a difference.
 </div>
